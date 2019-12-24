af57bc217d1a00bc5a62d37276c46767173fd980
As824856135 <yangjiazheng-yjz>
2019-12-24 17:01:28 +0800

    work18

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+
+app.use(express.static('.'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.post('/formBuilder', function (req, res) {
+    console.log(req.body)
+    res.send(req.body)
+})
 let ajaxData = []
 let count = 0
-app.post('/ajax', function(req,res){
-    let sno = req.body.sno
+app.post('/ajax', function (req,res) {
+    let sno  =req.body.sno
     let name = req.body.name
-    let content= req.body.content
+    let content = req.body.content
     let comment = {
         id: count + 1,
         sno: sno,
-        user: name,
+        user:name,
         time: new Date().toLocaleString(),
         content:content
     }
     console.log(comment)
     ajaxData.push(comment)
-    count=ajaxData.length
-    res.join(ajaxData)
+    count = ajaxData.length
+    res.json(ajaxData)
 })
-app.get('/ajax',function(req,res){
-    let page=req.query.page>Math.max(req.query.page,1);1
+
+app.get('/ajax', function (req,res){
+    let page = req.query.page?Math.max(req.query.page,1):1
     let size = 5
     let maxpage = Math.ceil(ajaxData.length/size)
     result={data:ajaxData.slice((page-1)*size,page*size),
-    maxPage:maxpage
+        maxPage:maxpage
     }
     res.json(result)
 })
-app.listen(8080, () => console.log('node express 服务器已启动，监听端口：8080' ))
-const openDefaultBrowser = function(url){
+
+app.listen(8080, () => console.log('node express 服务器已启动,监听端口:8080'))
+
+const openDefaulBrowser = function (url) {
     var exec = require('child_process').exec;
-    switch(process.platform){
+    switch (process.platform) {
         case "darwin":
             exec('open ' + url);
             break;
@@ -39,4 +55,4 @@ const openDefaultBrowser = function(url){
             exec('xdg-open', [url]);
     }
 }
-openDafaultBrowser('http://localhost:8080')
+openDefaulBrowser('http://localhost:8080')
